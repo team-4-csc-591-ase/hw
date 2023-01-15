93a460df0d87ef9d2082865f42502b8d5ff0206d
Parth <pthosan@ncsu.edu>
2023-01-15 16:08:00 -0500

    added num.py and sym.py

---
+++ src/num.py
@@ -0,0 +1,28 @@
+class newNum:
+    Seed = 937162211
+    n, mu, m2 = 0, 0, 0
+    lo, hi = sys.maxsize, -sys.maxsize
+
+    def add(self, n):
+        if n != "?":
+            self.n = self.n + 1
+            d = n - self.mu
+            self.mu = self.mu + (d / self.n)
+            self.m2 = self.m2 + (d * (n - self.mu))
+            self.lo = min(self.n, self.lo)
+            self.hi = max(self.n, self.hi)
+
+    def mid(self, x):
+        return self.mu
+
+    def div(self, x):
+        return (self.m2 < 0 or self.n < 2) and 0 or pow((self.m2 / (self.n - 1)), 0.5)
+
+    def rand(self, ho, li):
+        lo, hi = self.lo or 0, self.hi or 1
+        Seed = (16807 * self.Seed) % 2147483647
+        return lo + (hi - lo) * Seed / 2147483647
+
+    def rnd(self, n, nPlaces=3):
+        mult = pow(10, nPlaces)
+        return math.floor(n * mult + 0.5) / mult
---
+++ src/sym.py
@@ -0,0 +1,36 @@
+import math
+import sys
+
+
+class Newclass:
+    n = 0
+    has: dict = {} 
+    most = 0
+    mode = None
+
+    def rnd(self, nPlaces):
+        mult = math.pow(10, 3)
+        return math.floor((self.n * mult) + 0.5) / mult
+
+    def add(self, x):
+        if x != "?":
+            self.n = self.n + 1
+            if x in self.has:
+                y = self.has[x]
+            else:
+                y = 0
+            self.has[x] = 1 + y
+            if self.has[x] > self.most:
+                self.most, self.mode = self.has[x], x
+
+    def mid(self, x):
+        return self.mode
+
+    def div(self, x):
+        def fun(p):
+            return p * (math.log2(p))
+
+        e = 0
+        for k, v in self.has.items():
+            e = e + fun(v / self.n)
+        return -e
