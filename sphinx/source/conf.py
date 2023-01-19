87b2d453e69cec2f5d2901865e26164efdcd6893
Ritwik Tiwari <ritwiktiwari24@gmail.com>
2023-01-19 14:50:55 -0500

    Docs (#10)
    * add docs, poetry update
    
    * added files that were skipped
    
    * lint fix

---
+++ sphinx/source/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sys
+
+project = "ASE Homeworks"
+copyright = "2023, Parth, Ritwik, Urvashi"
+author = "Parth, Ritwik, Urvashi"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+sys.path.insert(0, os.path.abspath("../.."))
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".mypy_cache", "__pycache__"]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
