64e4221d067e8776a3ec8f05e04c46e70442f219
ukar <karurvashi@gmail.com>
2023-01-19 16:49:14 -0500

    added cli for main

--- src/config.py
+++ src/config.py
@@ -1,15 +1,12 @@
+from typing import Dict, Any
 from enum import Enum
-
-the = """USAGE:   script.lua  [OPTIONS] [-g ACTION]
-
-OPTIONS:
-  -d  --dump  on crash, dump stack = false
-  -g  --go    start-up action      = data
-  -h  --help  show help            = false
-  -s  --seed  random number seed   = 937162211
-
-ACTIONS:"""
 
 
 class CONSTS(Enum):
-    SEED = 937162211
+    SEED = "SEED",
+
+
+_CONSTS: Dict[str, Any] = {
+    CONSTS.SEED.name: 937162211,
+
+}
--- src/main.py
+++ src/main.py
@@ -1,2 +1,35 @@
-def hello_world():
-    return "Hello World"
+import argparse
+from src.config import CONSTS
+
+
+class Main:
+    def __init__(self):
+        parser = argparse.ArgumentParser(description="The LUA Project USAGE:")
+        parser.add_argument(
+            "-d",
+            "--dump",
+            help="on crash, dump stack",
+            required=False,
+            default=False,
+        )
+        parser.add_argument(
+            "-g",
+            "--go",
+            help="start-up action",
+            required=False,
+            default="data",
+        )
+        parser.add_argument(
+            "-s",
+            "--seed",
+            help="random number seed",
+            required=False,
+            default=937162211,
+        )
+        argument = parser.parse_args()
+        _CONSTS.update({CONSTS.SEED.name: argument.seed})
+        print(argument)
+
+
+if __name__ == "__main__":
+    Main()
