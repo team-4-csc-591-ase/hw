5c6bdb6922719737031656adefe82ece830d6bb5
Parth <pthosan@ncsu.edu>
2023-01-19 13:41:08 -0500

    modified test_rand

--- tests/test_rand.py
+++ tests/test_rand.py
@@ -24,10 +24,10 @@ def test_rand():
     )
 
 
-# def custom_assert_equals(val1, val2, msg=""):
-#     if val1 != val2:
-#         logging.error(msg)
-#         traceback.print_stack()
-#         return False
-#     else:
-#         return True
+def custom_assert_equals(val1, val2, msg=""):
+    if val1 != val2:
+        logging.error(msg)
+        traceback.print_stack()
+        return False
+    else:
+        return True
