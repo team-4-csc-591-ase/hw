1931e8f2d0ecd44bd996db05067a70333f885966
Urvashi Kar <karurvashi@gmail.com>
2023-01-19 14:39:18 -0500

    Misc functions and The eg implementation (#9)
    * Added eg(the)
    
    * Updated Main file
    
    * Added eg(the)
    
    * Updated Main file
    
    * Renamed file
    
    * latest
    
    * Modified Misc functions, Added test for the
    
    * Updated test the
    
    * Added eg(the)
    
    * Updated Main file
    
    * Renamed file
    
    * Added eg(the)
    
    * latest
    
    * Modified Misc functions, Added test for the
    
    * Updated test the

---
+++ src/config.py
@@ -0,0 +1,9 @@
+the = """USAGE:   script.lua  [OPTIONS] [-g ACTION]
+
+OPTIONS:
+  -d  --dump  on crash, dump stack = false
+  -g  --go    start-up action      = data
+  -h  --help  show help            = false
+  -s  --seed  random number seed   = 937162211
+
+ACTIONS:"""
---
+++ src/misc_functions.py
@@ -0,0 +1,67 @@
+import re
+
+
+# convert t to a string. sort named keys.
+def o(t: dict) -> str:
+    """
+
+    Args:
+        t: dict
+
+    Returns: str
+
+    """
+
+    result = "{"
+    keys = list(t.keys())
+    keys.sort()
+    for key in keys:
+        result += " :" + str(key) + " " + str(t[key])
+
+    result += "}"
+    return result
+
+
+# print t then return it
+def oo(t: dict) -> dict:
+    """
+
+    Args:
+        t: dict
+
+    Returns: dict
+
+    """
+    print(o(t))
+    return t
+
+
+def settings(s: str) -> dict:
+    """
+
+    Args:
+        s: String to be parsed
+
+    Returns: Dictionary of options from parsed string
+
+    """
+    return dict(re.findall(r"\n[\s]+[-][\S]+[\s]+[-][-]([\S]+)[^\n]+= ([\S]+)", s))
+
+
+def reformat(d: dict) -> dict:
+    """
+
+    Args:
+        d: Dict
+
+    Returns: Reformatted Dictionary
+
+    """
+    for key, val in d.items():
+        if val == "false":
+            d[key] = False
+        elif val == "true":
+            d[key] = True
+        elif val.isdigit():
+            d[key] = int(val)
+    return d
