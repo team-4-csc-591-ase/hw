63509ae0e2e7c1488b0e62fc762bf815c977a4c7
Ritwik Tiwari <ritwiktiwari24@gmail.com>
2023-01-19 14:48:58 -0500

    lint fix

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -3,14 +3,15 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import sys
-import os
 
-project = 'ASE Homeworks'
-copyright = '2023, Parth, Ritwik, Urvashi'
-author = 'Parth, Ritwik, Urvashi'
+project = "ASE Homeworks"
+copyright = "2023, Parth, Ritwik, Urvashi"
+author = "Parth, Ritwik, Urvashi"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -23,11 +24,11 @@ extensions = [
     "sphinx.ext.coverage",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".mypy_cache", "__pycache__"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
