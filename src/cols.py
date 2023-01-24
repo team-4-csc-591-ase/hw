96017a42473db6d89adc0dff9274e08a984a389d
Parth Thosani <43791395+thosaniparth@users.noreply.github.com>
2023-01-23 19:35:32 -0500

    added and modified files for hw2 (#19)
    * Modified NUM and SYM and Added COLS
    
    * Added Rows
    
    * Added Rows
    
    * lint fix
    
    Co-authored-by: Parth <pthosan@ncsu.edu>
    Co-authored-by: Ritwik Tiwari <ritwiktiwari24@gmail.com>

---
+++ src/cols.py
@@ -0,0 +1,40 @@
+from num import Num  # type: ignore
+from sym import Sym  # type: ignore
+
+
+class Cols:
+    def __init__(self, t) -> None:
+        self.names = t
+        self.all = []
+        self.x = []
+        self.y = []
+        self.klass = None
+
+        for name in t:
+            if name[0].isupper():
+                col = Num(t.index(name), name)
+            else:
+                col = Sym(t.index(name), name)
+
+            self.all.append(col)
+
+            if name[-1] != "X":
+                if name[-1] == "!":
+                    self.klass = name
+                if name[-1] == "+" or name[-1] == "-" or name[-1] == "!":
+                    self.y.append(name)
+                else:
+                    self.x.append(name)
+
+    def add(self, row) -> None:
+        """
+
+        Args:
+            row: int
+
+        Returns: None
+
+        """
+        total_columns = self.x.copy() + self.y.copy()
+        for col in total_columns:
+            col.add(row.cells[col.at])
--- src/num.py
+++ src/num.py
@@ -2,13 +2,25 @@ import sys
 
 
 class Num:
-    n, mu, m2 = 0, 0, 0
-    lo, hi = sys.maxsize, -sys.maxsize
+    def __init__(self, at=None, txt=None) -> None:
+        self.n, self.mu, self.m2 = 0, 0, 0
+        self.lo, self.hi = sys.maxsize, -sys.maxsize
+        if at:
+            self.at = at
+        else:
+            self.at = 0
+        if txt:
+            self.txt = txt
+        else:
+            self.txt = ""
+        if "-" in self.txt:
+            self.w = -1
+        else:
+            self.w = 1
 
     def add(self, n) -> None:
         """
         Args: n
-
         Return : None
         """
         if n != "?":
@@ -22,7 +34,6 @@ class Num:
     def mid(self) -> float:
         """
         Args: None
-
         Return : Float
         """
         return self.mu
@@ -30,10 +41,15 @@ class Num:
     def div(self) -> float:
         """
         Args: None
-
         Return : bool
         """
         if self.m2 < 0 or self.n < 2:
             return 0
         else:
             return (self.m2 / (self.n - 1)) ** 0.5
+
+    def rnd(self, x, n) -> float:
+        if x == "?":
+            return x
+        else:
+            return round(x, n)
