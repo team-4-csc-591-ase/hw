d5e5ae488c83415447b3851ad046923c48e2d180
Parth <pthosan@ncsu.edu>
2023-01-30 15:05:34 -0500

    updated mock_print

--- tests/test_the.py
+++ tests/test_the.py
@@ -18,6 +18,6 @@ def test_the(mock_print) -> None:
         ":file auto93.csv "
         ":go data "
         ":help False "
-        ":seed 937162211"
-        ":p 2}"
+        ":p 2 "
+        ":seed 937162211}"
     )
