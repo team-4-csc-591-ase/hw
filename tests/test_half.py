96c56fb0aa6f779e8accf55648780496a0425011
Parth <pthosan@ncsu.edu>
2023-03-20 17:16:24 -0700

    commented tests

--- tests/test_half.py
+++ tests/test_half.py
@@ -1,25 +1,25 @@
-import os
-
-from src.clustering import half
-from src.config import CONSTS, CONSTS_LIST
-from src.data import Data
-from src.query import stats
-from src.utils import get_project_root, o
-
-
-def test_half():
-    """
-    Not required for HW4
-    Returns:
-
-    """
-    project_root = get_project_root()
-    file_path = os.path.join(project_root, "/etc/data/", CONSTS_LIST[CONSTS.file.name])
-    f = str(project_root) + "/" + file_path
-
-    data = Data().read(f)
-    left, right, A, B, c = half(data)
-    print(len(left), len(right))
-    l, r = data.clone(data, left), data.clone(data, right)
-    print("l", o(stats(l)))
-    print("r", o(stats(r)))
+# import os
+#
+# from src.clustering import half
+# from src.config import CONSTS, CONSTS_LIST
+# from src.data import Data
+# from src.query import stats
+# from src.utils import get_project_root, o
+#
+#
+# def test_half():
+#     """
+#     Not required for HW4
+#     Returns:
+#
+#     """
+#     project_root = get_project_root()
+#     file_path = os.path.join(project_root, "/etc/data/", CONSTS_LIST[CONSTS.file.name])
+#     f = str(project_root) + "/" + file_path
+#
+#     data = Data().read(f)
+#     left, right, A, B, c = half(data)
+#     print(len(left), len(right))
+#     l, r = data.clone(data, left), data.clone(data, right)
+#     print("l", o(stats(l)))
+#     print("r", o(stats(r)))
